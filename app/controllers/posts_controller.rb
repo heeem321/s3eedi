b86c7b81a11c997a59f11265225e93ba2e2ff539
heeem321 <heeem321@hotmail.com>
2016-11-23 15:56:20 +0800

    created link between post and user

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,60 @@
 class PostsController < ApplicationController
+	before_action :find_post, only: [:show, :edit, :update, :destroy]
+	before_action :post_owner, only: [:edit, :update, :destroy]
+	
+	 def post_owner
+	     unless @post.user == current_user
+	     flash[:notice] = 'Access denied as you are not owner of this Post'
+	     redirect_to posts_path
+     end
+
+    end
+
 	def index
+		@posts = Post.all.order("created_at DESC")
 	end
+
+	def show
+	end
+
+	def new
+		@post = current_user.posts.build
+	end
+
+	def create
+		@post = current_user.posts.build(post_params)
+
+		if @post.save
+			redirect_to @post
+		else
+			render 'new'
+		end
+	end
+
+	def edit
+	end
+
+	def update
+		if @post.update(post_params)
+			redirect_to @post
+		else
+			render 'edit'
+		end
+	end
+
+	def destroy
+		@post.destroy
+		redirect_to root_path
+	end
+
+	private
+
+	def find_post
+		@post = Post.find(params[:id])
+	end
+
+	def post_params
+		params.require(:post).permit(:title, :content)
+	end
+
 end
